487275e4c0e59d5f5783036b61092604961aa933
Borbás Geri, eppz! <hello@eppz.eu>
2013-07-06 17:31:12 +0200

    Initial Commit

---
+++ eppz!coreData/EPPZAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  EPPZAppDelegate.h
+//  eppz!coreData
+//
+//  Created by Gardrobe on 7/6/13.
+//  Copyright (c) 2013 eppz!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class EPPZViewController;
+
+@interface EPPZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) EPPZViewController *viewController;
+
+@end
